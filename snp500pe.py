84d1276a37b72e0c9ae111e4a03ac19be7a3d40d
tombed <tomasz.bednarski@fitech.pl>
2025-05-13 21:58:07 +0200

    init hysteresis s evaluation

--- snp500pe.py
+++ snp500pe.py
@@ -12,8 +12,6 @@ import os
 import json
 
 
-
-
 @dataclass
 class Sigmoid:
     x0: float
@@ -25,6 +23,24 @@ class HysteresisLoop:
     def __init__(self, x0_rise: float, x0_fall: float, slope: float = 0.5):
         self.rising_sigmoid = Sigmoid(x0=x0_rise, k=slope)
         self.falling_sigmoid = Sigmoid(x0=x0_fall, k=slope)
+        self._historical_data = None
+        self.slope_estim_dates_num = 5
+
+    @property
+    def historical_data(self):
+        return self._historical_data
+
+    @historical_data.setter
+    def historical_data(self, data: List[Tuple[datetime, float]]):
+        if not isinstance(data, list):
+            raise ValueError("Historical data must be a list of tuples (datetime, float).")
+        for item in data:
+            if not (isinstance(item[0], datetime) and isinstance(item[1], float)):
+                raise ValueError("Each item in historical data must be a tuple (datetime, float).")
+
+        # Sort the data by date
+        sorted_data = sorted(data, key=lambda x: x[0], reverse=False)
+        self._historical_data = sorted_data
 
     @classmethod
     def sigmoid(cls, x: np.ndarray, min_val: float, max_val: float, k: float, x0: float):
@@ -42,16 +58,6 @@ class HysteresisLoop:
             float or np.ndarray: Output of the sigmoid function.
         """
         return min_val + (max_val - min_val) / (1 + np.exp(-k * (x - x0)))
-
-#product_dict = asdict(product)
-
-# Pass the dictionary to a function
-# def print_product_info(product_info: dict):
-#     for key, value in product_info.items():
-#         print(f"{key}: {value}")
-#
-# # Example usage
-# print_product_info(product_dict)
 
     def plot(self, x: np.ndarray):
         rise = asdict(self.rising_sigmoid)
@@ -73,7 +79,41 @@ class HysteresisLoop:
         plt.legend()
         plt.show()
 
+    def chceck_which_sigmoid(self, date: datetime) -> Sigmoid:
+        """
+        Check which sigmoid to use based on the date.
+        """
+        if date.month >= 6:
+            return self.rising_sigmoid
+        else:
+            return self.falling_sigmoid
 
+    def evaluate_non_stock_part(self, date: datetime, snp500_pe: float) -> float:
+        """
+        Evaluate the non-stock part based on the date and snp500_pe value.
+        """
+
+        month_begin = datetime(date.year, date.month, 1)
+        dates = [entry[0] for entry in self._historical_data]
+        pes = [entry[1] for entry in self._historical_data]
+
+        idx = dates.index(month_begin)
+        if month_begin == date:
+            idx = idx - 1
+
+        indexes = list(range(idx - self.slope_estim_dates_num, idx + 1))
+        slope, intercept = np.polyfit(np.arange(len(indexes)), np.array(pes)[indexes], 1)
+
+        if slope > 0 and snp500_pe > pes[idx]:
+            current_sigmoid = asdict(self.rising_sigmoid)
+            current_sigmoid["x"] = snp500_pe
+            return self.sigmoid(**current_sigmoid)
+        else: #TODO handle other 3 cases
+            current_sigmoid = self.falling_sigmoid
+
+
+
+        return 1
 
 def fetch_website_text(url: str) -> Optional[str]:
 
@@ -107,22 +147,9 @@ def extract_single_pe_ratio(text: str) -> Tuple[str, datetime]:
 
     return pe_ratio, date
 
-def extract_multiple_pe_ratios(text: str) -> List[Tuple[datetime, float]]:
-    # Regex to extract the date and value
-    date_pattern = r"\b\w{3} \d{1,2}, \d{4}\b"
-    value_pattern = r"\b\d+\.\d+\b"
 
-    # Extract dates and values
-    dates = re.findall(date_pattern, text)
-    values = re.findall(value_pattern, text)
 
-    assert len(dates) == len(values)
-    # dates = list(map(lambda x: dateutil.parser.parse(x), dates))
-    # values = list(map(float, values))
 
-    date_value_pairs = list(zip(dates, values))
-
-    return date_value_pairs
 
 
 class PERatioGetter:
@@ -139,11 +166,11 @@ class PERatioGetter:
         if os.path.exists(self.data_hist_file) and not self.force_fetch:
             with open(self.data_hist_file, "r") as file:
                 data = json.load(file)
-                return data
+                return self.change_to_date_float(data)
         else:
             text = fetch_website_text(self.PE_RATIO_HIST_URL)
             if text:
-                extracted_pe_ratios = extract_multiple_pe_ratios(text)
+                extracted_pe_ratios = self.extract_multiple_pe_ratios(text)
             else:
                 raise ValueError(f"Failed to fetch data from the website {self.PE_RATIO_HIST_URL}")
 
@@ -152,7 +179,39 @@ class PERatioGetter:
                 with open(self.data_hist_file, "w") as file:
                     json.dump(extracted_pe_ratios, file)
 
-            return extracted_pe_ratios
+            return self.change_to_date_float(extracted_pe_ratios)
+
+    @staticmethod
+    def extract_multiple_pe_ratios(text: str) -> List[Tuple[datetime, float]]:
+        # Regex to extract the date and value
+        date_pattern = r"\b\w{3} \d{1,2}, \d{4}\b"
+        value_pattern = r"\b\d+\.\d+\b"
+
+        # Extract dates and values
+        dates = re.findall(date_pattern, text)
+        values = re.findall(value_pattern, text)
+
+        assert len(dates) == len(values)
+        # dates = list(map(lambda x: dateutil.parser.parse(x), dates))
+        # values = list(map(float, values))
+
+        date_value_pairs = list(zip(dates, values))
+
+        return date_value_pairs
+
+    @staticmethod
+    def change_to_date_float(data: List[Tuple[str, str]]) -> List[Tuple[datetime, float]]:
+        """
+        Convert date strings to datetime objects and values to floats.
+        """
+        date_value_pairs = []
+        for date_str, value_str in data:
+            date = dateutil.parser.parse(date_str)
+            value = float(value_str)
+            date_value_pairs.append((date, value))
+        return date_value_pairs
+
+
 
 
 
@@ -188,9 +247,12 @@ if __name__ == "__main__":
 
     hysteresis_loop = HysteresisLoop(x0_rise, x0_fall, slope=k)
     hysteresis_loop.plot(x)
+    hysteresis_loop.historical_data = extracted
+    non_stock_part = hysteresis_loop.evaluate_non_stock_part(datetime(2023, 10, 1), 25)
 
 
-    # check_values = np.arange(10, 40, 2.5)
+
+    check_values = np.arange(10, 40, 2.5)
     # results = sigmoid(check_values, min_val, max_val, k, x0_rise)
     # for x_, y_ in zip(check_values, results):
     #     print(x_, y_)
