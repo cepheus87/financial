014abfc621f5dcfd2e2516982c5460288c1a0561
tombed <tomasz.bednarski@fitech.pl>
2025-09-08 21:46:43 +0200

    fix commision calculation for xtb

--- record_entry.py
+++ record_entry.py
@@ -1,6 +1,7 @@
 import argparse
+import math
 
-def get_entry_values(full_cost: float, commission: float, price_in_currency: float, units: float):
+def get_entry_values(full_cost: float, commission: float, price_in_currency: float, units: float, sell: bool):
 
     """
     Calculate the entry values for an investment.
@@ -10,36 +11,44 @@ def get_entry_values(full_cost: float, commission: float, price_in_currency: flo
     - commission: The commission in value (PLN) or percentage (decimal).
     - price_in_currency: The price of a single unit in the original currency.
     - units: The number of assets.
+    - sell: Boolean indicating if the operation is a sell.
 
     """
 
 
-
-    if commission < 1:
-        cost_no_commission = full_cost / (1 + commission)
-        commission_in_PLN = full_cost * commission
+    if sell:
+        cost_no_commission = full_cost
+        currency_rate = cost_no_commission / (units * price_in_currency)
+        commission_in_PLN = commission * (cost_no_commission * (1 + commission))
     else:
-        cost_no_commission = full_cost - commission
-        commission_in_PLN = commission
+        if commission < 1:
+            cost_no_commission = full_cost / (1 + commission)
+            commission_in_PLN = get_commision_value(commission, full_cost)
 
-    currency_rate = cost_no_commission / (units * price_in_currency)
+        else:
+            cost_no_commission = full_cost - commission
+            commission_in_PLN = commission
+
+        currency_rate = full_cost / (units * price_in_currency)
 
     print(f"Currency rate: {currency_rate:.5f}")
-    print(f"Commission PLN: {commission_in_PLN:.5f}")
+    print(f"Commission PLN: {commission_in_PLN:.2f}")
 
+def get_commision_value(commission: float, full_cost: float):
+    commission_in_PLN = math.ceil(commission * full_cost * 100) / 100
+    return commission_in_PLN
 
 if __name__ == "__main__":
 
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-fc", "--full-cost", type=float, required=True, help="Full cost in PLN")
-    parser.add_argument("-c", "--commission", type=float, required=True, help="Commission in value (PLN) or "
-                                                                              "percentage ("
-                                                                        "decimal)")
+    parser.add_argument("-c", "--commission", type=float, required=False, default=0.00516, help="Commission in value (PLN) or percentage (decimal)")
     parser.add_argument("-pic", "--price-in-currency", type=float, required=True, help="Price of single unit in "
                                                                                       "original "
                                                                                "currency")
     parser.add_argument("-u", "--units", type=float, required=True, help="Units of assets")
+    parser.add_argument("-s", "--sell", action="store_true", help="Indicate if the operation is a sell")
     args = parser.parse_args()
 
-    get_entry_values(args.full_cost, args.commission, args.price_in_currency, args.units)
+    get_entry_values(args.full_cost, args.commission, args.price_in_currency, args.units, args.sell)
